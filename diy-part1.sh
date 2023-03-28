4e2b1775d0203bf060d4da4957d8e71536640a4f
Mibore99 <854733501@qq.com>
2023-03-28 21:31:19 +0800

    test2

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,4 @@ svn export https://github.com/friendlyarm/friendlywrt/trunk/package/boot/uboot-r
 #rm -rf target/linux/rockchip/patches-5.10
 #svn export https://github.com/friendlyarm/friendlywrt/trunk/target/linux/rockchip/patches-5.10
 #svn export https://github.com/openwrt/openwrt/trunk/target/linux/rockchip/patches-5.10/105-nanopi-r4s-sd-signalling.patch
+svn cp https://github.com/coolsnowwolf/lede/trunk/target/linux/rockchip/patches-5.10//991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch /target/linux/rockchip/patches-5.10//991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
