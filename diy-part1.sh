2a997477876f302bd0015405edd8511c86eee7bd
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2023-03-28 21:45:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,8 +17,9 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 rm -rf package/boot/uboot-rockchip/patches
-svn export https://github.com/friendlyarm/friendlywrt/trunk/package/boot/uboot-rockchip/patches
+#svn export https://github.com/friendlyarm/friendlywrt/trunk/package/boot/uboot-rockchip/patches
 #rm -rf target/linux/rockchip/patches-5.10
 #svn export https://github.com/friendlyarm/friendlywrt/trunk/target/linux/rockchip/patches-5.10
 #svn export https://github.com/openwrt/openwrt/trunk/target/linux/rockchip/patches-5.10/105-nanopi-r4s-sd-signalling.patch
 svn cp https://github.com/coolsnowwolf/lede/trunk/target/linux/rockchip/patches-5.10//991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch /target/linux/rockchip/patches-5.10//991-arm64-dts-rockchip-add-more-cpu-operating-points-for.patch
+svn cp https://github.com/friendlyarm/friendlywrt/trunk/package/boot/uboot-rockchip/patches package/boot/uboot-rockchip/patches
