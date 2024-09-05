bcc2080a71ecf8aacaf9f26dee95c6c8147f4594
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2024-09-05 10:55:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,3 @@
 curl -L -o feeds/packages/lang/golang/golang/Makefile https://github.com/openwrt/packages/raw/master/lang/golang/golang/Makefile
-echo "/etc/uci-defaults/70-rootpt-resize" >> /etc/sysupgrade.conf
-echo "/etc/uci-defaults/80-rootfs-resize" >> /etc/sysupgrade.conf
+sudo echo "/etc/uci-defaults/70-rootpt-resize" >> /etc/sysupgrade.conf
+sudo echo "/etc/uci-defaults/80-rootfs-resize" >> /etc/sysupgrade.conf
