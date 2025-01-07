c790072c8b19834433b9cc0e277ae934f6c551ba
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2025-01-07 08:27:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,3 @@
-echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5' >>feeds.conf.default
 
