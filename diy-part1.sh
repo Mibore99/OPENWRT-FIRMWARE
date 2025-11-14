f83a4054d1b4c52dd9806d2318e1159757150eb0
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2025-11-14 22:07:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,2 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
 echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5' >>feeds.conf.default
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.82.0/g' feeds/packages/lang/rust/Makefile
