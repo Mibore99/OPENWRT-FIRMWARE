1340bb9b950148b55cbe9d36858bf719da943201
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2025-11-22 15:29:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,6 @@
-sed -i 's|git.openwrt.org|github.com/openwrt|g' feeds.conf.default
+sed -i -e 's|https://git.openwrt.org/feed/\(.*\)|https://github.com/openwrt/\1|g' \
+       -e 's|https://git.openwrt.org/project/\(.*\)|https://github.com/openwrt/\1|g' \
+       feeds.conf.default
 
 echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
 echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5' >>feeds.conf.default
