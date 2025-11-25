44763f11b66209b901e8527270acb5b995ba1b95
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2025-11-26 00:50:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,1 @@
-find feeds/passwall_packages -name "go.mod" -exec sed -i 's/go 1.24/go 1.23/g' {} +
+sed -i '/call BuildPackage/i define Build/Prepare\n\t$(call Build/Prepare/Default)\n\tsed -i "s/go 1.24/go 1.23/g" $(PKG_BUILD_DIR)/go.mod\nendef\n' feeds/passwall_packages/geoview/Makefile
