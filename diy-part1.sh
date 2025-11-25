43a4f119a3c83f9b87efa6365af94c922329f600
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2025-11-25 17:35:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,6 +2,5 @@ sed -i -e 's|https://git.openwrt.org/feed/\(.*\)|https://github.com/openwrt/\1|g
        -e 's|https://git.openwrt.org/project/\(.*\)|https://github.com/openwrt/\1|g' \
        feeds.conf.default
 
-echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
