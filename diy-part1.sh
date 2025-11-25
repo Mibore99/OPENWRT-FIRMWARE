f879963fb1188589265706fb63af365a1c91ecd9
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2025-11-25 14:17:10 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,5 +2,6 @@ sed -i -e 's|https://git.openwrt.org/feed/\(.*\)|https://github.com/openwrt/\1|g
        -e 's|https://git.openwrt.org/project/\(.*\)|https://github.com/openwrt/\1|g' \
        feeds.conf.default
 
-echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
 echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
