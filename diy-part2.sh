94856698bec86994d831f51af2ef4ae5e5cb187b
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2025-11-26 12:37:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,5 @@
-find feeds/passwall_packages -name "Makefile" | xargs grep -l "golang-package.mk" | while read makefile; do
-    if ! grep -q "define Build/Prepare" "$makefile"; then
-        perl -i -pe 'print "define Build/Prepare\n\t\$(call Build/Prepare/Default)\n\tsed -i \"s/go 1.24/go 1.23/g\" \$(PKG_BUILD_DIR)/go.mod\nendef\n\n" if /^call BuildPackage/' "$makefile"
+find feeds/passwall_packages -name "Makefile" | xargs grep -l "golang-package.mk" | while read -r mkfile; do
+    if ! grep -q "define Build/Prepare" "$mkfile"; then
+        sed -i "/golang-package.mk/a define Build/Prepare\n\t\$(call Build/Prepare/Default)\n\tsed -i 's/go 1.24/go 1.23/g' \$(PKG_BUILD_DIR)/go.mod\nendef" "$mkfile"
     fi
 done
