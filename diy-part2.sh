549c2db33854560115a9b167f4a28c0d6bf9464e
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2025-11-26 15:19:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,5 @@
-find feeds/passwall_packages -name "Makefile" | xargs grep -l "golang-package.mk" | while read -r mkfile; do
-    if ! grep -q "define Build/Prepare" "$mkfile"; then
-        sed -i "/golang-package.mk/a define Build/Prepare\n\t\$(call Build/Prepare/Default)\n\tsed -i 's/go 1.24/go 1.23/g' \$(PKG_BUILD_DIR)/go.mod\nendef" "$mkfile"
-    fi
-done
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/openwrt/packages.git -b master --depth 1 temp_packages
+mv temp_packages/lang/golang feeds/packages/lang/
+rm -rf temp_packages
+./scripts/feeds install -a
