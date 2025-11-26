4f2d7fe9efcd09234c4e1803439f7fd7f671160a
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2025-11-26 10:02:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,5 @@
-sed -i '/call BuildPackage/i define Build/Prepare\n\t$(call Build/Prepare/Default)\n\tsed -i "s/go 1.24/go 1.23/g" $(PKG_BUILD_DIR)/go.mod\nendef\n' feeds/passwall_packages/geoview/Makefile
+find feeds/passwall_packages -name "Makefile" | xargs grep -l "golang-package.mk" | while read makefile; do
+    if ! grep -q "define Build/Prepare" "$makefile"; then
+        perl -i -pe 'print "define Build/Prepare\n\t\$(call Build/Prepare/Default)\n\tsed -i \"s/go 1.24/go 1.23/g\" \$(PKG_BUILD_DIR)/go.mod\nendef\n\n" if /^call BuildPackage/' "$makefile"
+    fi
+done
